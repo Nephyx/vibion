6834c4bfa451950d3de9e5dd9f23a3985a183a12
Maroš Kováč <maros1305@gmail.com>
2019-04-12 23:01:29 +0200

    Initial backend skeleton

---
+++ backend/src/main/java/com/line21k/vibion/VibionApplication.java
@@ -0,0 +1,13 @@
+package com.line21k.vibion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VibionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VibionApplication.class, args);
+	}
+
+}
