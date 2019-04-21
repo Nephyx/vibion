dbeab1b8dfcba43aaa06502d16ef5e944ac3d83a
Maroš Kováč <maros1305@gmail.com>
2019-04-21 23:05:35 +0200

    Changed JDK version, added Spring Cloud

--- backend/src/main/java/com/line21k/vibion/VibionApplication.java
+++ backend/src/main/java/com/line21k/vibion/VibionApplication.java
@@ -2,8 +2,10 @@ package com.line21k.vibion;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class VibionApplication {
 
 	public static void main(String[] args) {
